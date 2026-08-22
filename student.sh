7df2088d7bc7462b0c5d2dcf9d50af073cbb9d33
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-08-22 12:40:05 +0530

    Create student.sh

---
+++ student.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# UNIX Practical 6
+# CAT Command
+
+# Student Name:
+# Register Number:
+
+# TASK:
+#
+# 1. Create newfile.txt with the following content:
+#    Hello Linux
+#    This is a cat command practical.
+#
+# 2. Create file1.txt with:
+#    First file
+#
+# 3. Create file2.txt with:
+#    Second file
+#
+# 4. Concatenate file1.txt and file2.txt.
+#
+# 5. Create existingfile.txt with:
+#    Existing file
+#
+# 6. Append the contents of file1.txt to existingfile.txt.
+#
+# 7. The required files must be created using the cat command.
+#
+# Write your commands below this line:
+
+
+
