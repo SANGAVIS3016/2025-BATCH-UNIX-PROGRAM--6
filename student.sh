24fd0d4c3819de3c45574ef52e566d49176b812e
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-08-29 14:04:33 +0530

    Update student.sh

--- student.sh
+++ student.sh
@@ -6,28 +6,29 @@
 # Student Name:
 # Register Number:
 
-# TASK:
-#
-# 1. Create newfile.txt with the following content:
-#    Hello Linux
-#    This is a cat command practical.
-#
-# 2. Create file1.txt with:
-#    First file
-#
-# 3. Create file2.txt with:
-#    Second file
-#
-# 4. Concatenate file1.txt and file2.txt.
-#
-# 5. Create existingfile.txt with:
-#    Existing file
-#
-# 6. Append the contents of file1.txt to existingfile.txt.
-#
-# 7. The required files must be created using the cat command.
-#
-# Write your commands below this line:
+# 1. Create newfile.txt
+cat > newfile.txt << EOF
+Hello Linux
+This is a cat command practical.
+EOF
 
+# 2. Create file1.txt
+cat > file1.txt << EOF
+First file
+EOF
 
+# 3. Create file2.txt
+cat > file2.txt << EOF
+Second file
+EOF
 
+# 4. Concatenate file1.txt and file2.txt
+cat file1.txt file2.txt > concatenated.txt
+
+# 5. Create existingfile.txt
+cat > existingfile.txt << EOF
+Existing file
+EOF
+
+# 6. Append file1.txt to existingfile.txt
+cat file1.txt >> existingfile.txt
